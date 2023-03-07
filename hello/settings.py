f59bd2a001a723c3aecb34a1c74163c2379cc791
sanjeev929 <sanjeevsanju929@gmail.com>
2023-03-07 07:42:44 +0530

    add change

--- hello/settings.py
+++ hello/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6&8=hcl(lj_vzqd=1c24k*^$ef)f6ek^3g(*k-f@u@=q#a#i$%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
